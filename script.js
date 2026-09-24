7c1f270a0d75f500d9ed17da2480030617d01bb9
lovsh31 <283480261+lovsh31@users.noreply.github.com>
2026-09-24 20:21:07 +0200

    Style the UI
    Style the page using css
    Add a button to "Play Again"

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 const display = document.querySelector("#display");
-const score = document.querySelector("#score");
+const scoreU = document.querySelector("#scoreU");
+const scoreC = document.querySelector("#scoreC");
 const message = document.querySelector("#message");
 
-
-
-
+const btnNewGame = document.createElement("button"); //Button to start a new game
+const paraFinal = document.createElement("p");
 
 
 
@@ -29,24 +29,6 @@ function getComputerChoice(){
 }
 
 
-//Ask the user to enter his answer between "Rock,Paper or scissors" and place his answer in userChoice
-//Format it to lower case and check it is an acceptable answer
-//IF it is return the value
-//ELSE Warn the user and Ask the user to enter his answer again 
-function getUserChoice(){
-    let userPrompt = prompt("Enter rock, paper or scissors");
-    while(userPrompt!==null){
-        let lower = userPrompt.toLowerCase();
-        if(lower==="rock" || lower==="paper" || lower==="scissors"){
-            return lower;
-         }else{
-            userPrompt = prompt("Enter rock, paper or scissors");
-        }    
-    }   
-    return false;        
-}
-
-
 //Create a function playGame that starts 5 round and decide a winner
 function playGame(){
     let countRound=0;
@@ -54,49 +36,62 @@ function playGame(){
     //Create two variable to keep score
     let userScore = 0;
     let computerScore = 0;
+    //Create a variable computerChoice that contain a empty string
+    let computerChoice = "";
 
+    scoreU.textContent = userScore;       
+    scoreC.textContent = computerScore;
+    message.textContent = "";
     
+    if(btnNewGame.textContent && paraFinal.textContent){ //If new game, we remove endgame button and text
+        display.removeChild(btnNewGame);
+        display.removeChild(paraFinal);
+    }
 
-
-    function playRound(userSelection){
+    function playRound(userChoice){
         if(countRound<5){
+            countRound++;
             computerChoice = getComputerChoice();
-            if(userSelection==="rock"){
+            if(userChoice==="rock"){
                 switch(computerChoice){
                     case "rock":
                         message.textContent = "It's a tie!";
-                        score.textContent = `You: ${userScore} Computer: ${computerScore}`;                        
+                        scoreU.textContent = userScore;       
+                        scoreC.textContent = computerScore;                 
                         break;
                     case "paper":
                         message.textContent = "You lose! Paper beats Rock";
                         computerScore++;
-                        score.textContent = `You: ${userScore} Computer: ${computerScore}`;
-
+                        scoreU.textContent = userScore;       
+                        scoreC.textContent = computerScore;  
                         break;
                     case "scissors":
                         message.textContent = "You win! Rock beats Scissors";
                         userScore++;
-                        score.textContent = `You: ${userScore} Computer: ${computerScore}`;
-
+                        scoreU.textContent = userScore;       
+                        scoreC.textContent = computerScore;  
                         break;
                 }
-            }else if(userSelection==="paper"){
+            }else if(userChoice==="paper"){
                 switch(computerChoice){
                     case "rock":
                         message.textContent = "You win! Paper beats Rock";
                         userScore++;
-                        score.textContent = `You: ${userScore} Computer: ${computerScore}`;
+                        scoreU.textContent = userScore;       
+                        scoreC.textContent = computerScore;  
 
                         break;
                     case "paper":
                         message.textContent = "It's a tie!";
-                        score.textContent = `You: ${userScore} Computer: ${computerScore}`;
+                        scoreU.textContent = userScore;       
+                        scoreC.textContent = computerScore;  
 
                         break;
                     case "scissors":
                         message.textContent = "You lose! Scissors beats Paper";
                         computerScore++;
-                        score.textContent = `You: ${userScore} Computer: ${computerScore}`;
+                        scoreU.textContent = userScore;       
+                        scoreC.textContent = computerScore;  
                         break;
                 }
             }else{
@@ -104,54 +99,57 @@ function playGame(){
                     case "rock":
                         message.textContent = "You lose! Rock beats Scissors";
                         computerScore++;
-                        score.textContent = `You: ${userScore} Computer: ${computerScore}`;
-
+                        scoreU.textContent = userScore;       
+                        scoreC.textContent = computerScore;
                         break;
                     case "paper":
-                        console.log("You win! Scissors beats Paper");
+                        message.textContent = "You win! Scissors beats Paper";
                         userScore++;
-                        score.textContent = `You: ${userScore} Computer: ${computerScore}`;
+                        scoreU.textContent = userScore;       
+                        scoreC.textContent = computerScore;  
 
                         break;
                     case "scissors":                    
                         message.textContent = "It's a tie!";
-                        score.textContent = `You: ${userScore} Computer: ${computerScore}`;
+                        scoreU.textContent = userScore;       
+                        scoreC.textContent = computerScore;  
                         break;                
                 }
             }
-        }else{
+        }
+        if(countRound===5){
             const finalMessage = (userScore>computerScore)?"You are the grand winner!":
             (userScore<computerScore)?"Game Over! You lose":
             "The game is a TIE!";
-            console.log(finalMessage);
+
+            //Show finale message in a big fat font
+            paraFinal.textContent = finalMessage;            
+            paraFinal.classList.add("messageFinal");
+            display.appendChild(paraFinal);
+
+            //Add a button for a new game
+            btnNewGame.textContent = "Play Again";
+            display.appendChild(btnNewGame);
+            btnNewGame.addEventListener("click", playGame);
+            /*display.removeChild(btnNewGame);
+            display.removeChild(paraFinal);*/
         }
     }
 
-        
-    //Create a variable computerChoice that contain a empty string
-    let computerChoice = "";
-
-    /*Create a variable userChoice that contain a empty string
-    let userChoice = getUserChoice();*/
-    
+   
     //event listener on button to start a round
-    rock.addEventListener("click", ()=>{
-        playRound("rock");
-        countRound++;
+    rock.addEventListener("click", () => {
+        if (countRound<5)
+            playRound("rock");
     });
     scissors.addEventListener("click", ()=>{
-        playRound("scissors");
-        countRound++;
+        if (countRound<5)
+            playRound("scissors");
     });
     paper.addEventListener("click", ()=>{
-        playRound("paper");
-        countRound++;
+        if (countRound<5)
+            playRound("paper");
     });
-        
-    if (userQuit){
-        const finalMessage = "Don't be a chicken! Game Over";
-        console.log(finalMessage);
-    }
 }
 playGame();
 
